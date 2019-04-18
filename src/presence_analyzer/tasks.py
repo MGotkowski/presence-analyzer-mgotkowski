e1d9a91bdbdfb2f1ecdd4055ec8f7256b9b16337
Mateusz Gotkowski <mateusz.gotkowski.91@gmail.com>
2019-04-18 13:25:39 +0200

    [PMG-16] task works with given data

--- src/presence_analyzer/tasks.py
+++ src/presence_analyzer/tasks.py
@@ -1,3 +1,5 @@
+import time
+
 from celery import Celery
 from main import app
 
@@ -26,15 +28,19 @@ def send_emails():
     """
     data = mails_handling()
     for user in data:
+        mean_time = time.strftime(
+            '%H:%M:%S',
+            time.gmtime(data[user]['mean_time'])
+        )
+
+        body = 'Your mean daily work time is {}'.format((user, mean_time))
+
         with app.app_context():
-                # user['email'],
-                # (user, data[user]['mean_time'])
             msg = Message(
                 subject='Missing work hours.',
-                recipients=['lupemo@direct-mail.info'],
+                recipients=['wutafaga@dreamcatcher.email'],
                 sender='myapp_stx@o2.pl',
-                body="Your mean work hours is dupa"
+                body=body
             )
             mail.send(msg)
-
     return 'sent'
